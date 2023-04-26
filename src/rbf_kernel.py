eb850d397a4257ec8f4d9f595541d3838914b0d2
Andrea Mastropietro <andrea.mastropietro@gmail.com>
2023-04-26 09:34:33 +0200

    Implemented core RBF kernel functions and exact Shapley

--- src/rbf_kernel.py
+++ src/rbf_kernel.py
@@ -3,11 +3,12 @@ import math
 import numpy as np
 from scipy import sparse
 from scipy.special import binom
+from itertools import combinations
 
 ##############my functions#####################
 
 #TODO: change gamma with sigma
-def rbf_kernel(matrix_a: np.ndarray, matrix_b: np.ndarray, gamma: float = 1.0):
+def rbf_kernel_matrix(matrix_a: np.ndarray, matrix_b: np.ndarray, gamma: float = 1.0, sigma: float = None):
     """Calculates the RBF kernel between two matrices and returns a similarity matrix.
 
     Parameters
@@ -18,16 +19,82 @@ def rbf_kernel(matrix_a: np.ndarray, matrix_b: np.ndarray, gamma: float = 1.0):
         matrix b
     gamma: float
         gamma parameter of the RBF kernel
+    sigma: float
+        sigma parameter of the RBF kernel, if not None, gamma is calculated as 1/(2*sigma**2).
+        Any value given to gamma when sigma is not None is ignored.
     Returns
     -------
         np.ndarray
     """
     norm_1 = np.multiply(matrix_a, matrix_a).sum(axis=1)
     norm_2 = np.multiply(matrix_b, matrix_b).sum(axis=1)
-    distance = np.add.outer(norm_1, norm_2.T) - 2 * matrix_a.dot(matrix_b.transpose())
-    return np.exp(-gamma * distance)
+    distance_squared = np.add.outer(norm_1, norm_2.T) - 2 * matrix_a.dot(matrix_b.transpose())
+    print(distance_squared)
+    if sigma is not None:
+        gamma = 1 / (2 * sigma ** 2)
+    return np.exp(-gamma * distance_squared)
+
+def rbf_kernel(x: np.ndarray, y: np.ndarray, gamma: float = 1.0, sigma: float = None):
+    euclidean_distance = np.linalg.norm(x - y, ord=2)
+    print(euclidean_distance**2)
+    if sigma is not None:
+        gamma = 1 / (2 * sigma ** 2)
+    return np.exp(-gamma * (euclidean_distance**2))
 
 
+
+class ExactRBFShapleyComputation:
+    """Calculates the Shapley value according to the Shapley formalism for the RBF kernel.
+        (Enumerates all possible coalitions, exponential cost!)
+    """
+    def __init__(self, ref_arr):
+        self.ref_arr = ref_arr
+    def rbf_kernel_value(self, x):
+        """Calculates the similarity of an instance x to the initialized vector `ref_arr`.
+        This can be treated equivalent to a prediction of the similarity to ref_arr given x.
+        Hence, we can explain the similarity with SHAP.
+        """
+        return rbf_kernel_matrix(x, self.ref_arr.reshape((1, -1)))
+
+    def shapley_values(self, x):
+        ref = self.ref_arr
+        assert x.shape == ref.shape
+        shapley_vector = []
+        for f_idx, (f_x, f_ref) in enumerate(zip(x, ref)):
+            # Initializing the Shapley value as 0
+            shapley_value = 0
+            # Removing the assessed feature from the vectors
+            remaining_f_x = np.delete(x, f_idx)
+            remaining_f_ref = np.delete(ref, f_idx)
+
+            # Iterating over all coalition sizes
+            for coal_size in np.arange(0, x.shape[0]+1):
+                if coal_size == 0: # if empty coalition:
+                    if f_x == f_ref == 0:
+                        # if the feature is absent in both instances, it does not affect the RBF kernel value.
+                        # shapley value += 0 , or simply skip
+                        continue
+                    shapley_value += ((f_x*f_ref) -0) * inv_muiltinom_coeff(x.shape[0], coal_size)
+                    continue
+                feature_indices = np.arange(0, remaining_f_x.shape[0], dtype=int)
+
+                #Creating all feature combinations as possible coalitions.
+                for sel_features in combinations(feature_indices, r=coal_size):
+                    sel_features = np.array(sel_features)
+                    coal_x = remaining_f_x[sel_features]  # Coalition without assessed feature
+                    coal_ref = remaining_f_ref[sel_features]  # Coalition without assessed feature
+                    coal_x_fi = np.hstack([coal_x, [f_x]]) # Coalition with assessed feature
+                    coal_ref_refi = np.hstack([coal_ref, [f_ref]])  # Coalition with assessed feature
+                    if np.sum(coal_x_fi + coal_ref_refi) == 0:
+                        # if the all features (including the assessed feature) are absent in both instances, they do not affect the Tanimoto similarity.
+                        # shapley value += 0 , or simply skip
+                        continue
+                    if np.sum(coal_x + coal_ref) == 0: # if empty coalition (or coalition form only absent features), set subtracting term to 0
+                        shapley_value += (rbf_kernel(coal_x_fi, coal_ref_refi)-0) * inv_muiltinom_coeff(x.shape[0], coal_size)
+                    else:
+                        shapley_value += (rbf_kernel(coal_x_fi, coal_ref_refi) - rbf_kernel(coal_x, coal_ref)) * inv_muiltinom_coeff(x.shape[0], coal_size)
+            shapley_vector.append(shapley_value)
+        return np.array(shapley_vector)
 ####################################################################################
 
 def tanimoto_similarity_sparse(matrix_a: sparse.csr_matrix, matrix_b: sparse.csr_matrix):
